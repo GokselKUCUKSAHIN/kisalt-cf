006e976aff0a2d16bd0e854660c586e0c63b1030
gokselkucuksahin <gokselkucuksahin@gmail.com>
2021-10-31 18:48:33 +0300

    mongoDb error handler added.

--- db-test/index.js
+++ db-test/index.js
@@ -4,10 +4,15 @@ const MONGO_URI = process.env.MONGO_URI;
 
 const client = mongodb.MongoClient;
 const uri = MONGO_URI;
+const database = "newDB";
+const table = "users";
 
 client.connect(uri, (err, client) => {
-  const db = client.db("newDB");
-  const collection = db.collection("users");
+  if (!!err) {
+    return console.log("Database refused to connection.\nDetails:", err.errmsg);
+  }
+  const db = client.db(database);
+  const collection = db.collection(table);
   const query = {};
   const cursor = collection.find(query);
   cursor.forEach(doc => console.log(doc), err => client.close());
