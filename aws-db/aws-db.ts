8866dffd27aea4b2a956d97bd5837e09eb4c15b5
gokselkucuksahin <gokselkucuksahin@gmail.com>
2022-01-26 04:52:06 +0300

    aws-db tested and document created.

--- aws-db/aws-db.ts
+++ aws-db/aws-db.ts
@@ -1,6 +1,11 @@
+import {mongoDbDriverFactory, using} from "moosedb";
+
 class Main {
   static async main() {
-
+    using(await mongoDbDriverFactory("mongodb://superUser:pass123@13.48.27.102:27017"), async driver => {
+      const collection = await driver.db("compas").get("hello");
+      console.log(await collection.insertOne({id: 42, status: "OK"}));
+    });
   }
 }
 
